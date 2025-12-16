02b7250cb89ab4f0dc5d621ee337cd118a6e246e
acejun01 <acejun01@gmail.com>
2025-12-15 19:48:05 -0700

    Commit these 2 files

--- src/main/java/com/example/unitedportraits/order/Order.java
+++ src/main/java/com/example/unitedportraits/order/Order.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 public class Order {
-    //hello world 9
+    //hello world 10acles
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
--- src/main/java/com/example/unitedportraits/student/Student.java
+++ src/main/java/com/example/unitedportraits/student/Student.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class Student {
-
+    //this is the student class
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "student_id")
